407646784d56922a4da00a7302951065d4d5ac19
testing.seraphic <testing.seraphic@gmail.com>
2024-11-12 18:07:47 +0530

    font chnages

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
             path: "Sources/TeleVideoCall" , // Path to your source code folder
             resources: [
                 .process("Sources/TeleVideoCall/TelemechanicVideoMain.storyboard"),
-                .process("Sources/Font/ttf/OverusedGrotesk-SemiBold.ttf")
+                .process("Sources/TeleVideoCall/Font/ttf/OverusedGrotesk-SemiBold.ttf")
                        ]
         ),
         .testTarget(
