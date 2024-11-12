c9599c41713d270512ad9781de5805524bfdc20f
testing.seraphic <testing.seraphic@gmail.com>
2024-11-12 16:13:05 +0530

    manager changes

---
+++ Sources/TeleVideoCall/Manager/CallManager.swift
@@ -0,0 +1,33 @@
+//
+//  CallManager.swift
+//  TelemechanicVideoCallPluginDemoSPM
+//
+//  Created by Apple on 12/11/24.
+//
+
+import Foundation
+
+class CallManager {
+    static let shared = CallManager()
+    let debugSettingsModel: DebugSettingsModel = DebugSettingsModel()
+    
+    private init() {
+    }
+    
+    func navigateToVideoCallScreen(participantName: String,channelName: String) {
+        
+        MeetingModule.shared().prepareMeeting(meetingId: channelName,
+                                              selfName: participantName,
+                                              overriddenEndpoint: self.debugSettingsModel.endpointUrl,
+                                              primaryExternalMeetingId: self.debugSettingsModel.primaryExternalMeetingId) { success in
+            DispatchQueue.main.async {
+                if !success {
+                    // Handle failure to prepare the meeting
+//                    let alert = UIAlertController(title: "Error", message: "Please enter different Meeting ID", preferredStyle: .alert)
+//                    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+//                    self.present(alert, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+}
