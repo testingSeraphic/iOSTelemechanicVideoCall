5fa4bf53f6db1bd5c3517081788dfeb781a6580a
Manpreet <manpreet.seraphic@gmail.com>
2024-12-13 17:39:24 +0530

    Latest changes

--- Sources/TeleVideoCall/FontManager.swift
+++ Sources/TeleVideoCall/FontManager.swift
@@ -9,27 +9,24 @@
 import UIKit
 import CoreText
 
-public struct FontManager {
-    public static func initializeFonts() {
-        // Find all .ttf font files in the Font/ttf folder
-        if let fontURLs = Bundle.module.urls(forResourcesWithExtension: "ttf", subdirectory: "Font/ttf") {
-            print("FontURLs==",fontURLs)
-            print("FontURLsCount==",fontURLs.count)
-            for fontURL in fontURLs {
-                if let fontDataProvider = CGDataProvider(url: fontURL as CFURL),
-                   let font = CGFont(fontDataProvider) {
-                    var error: Unmanaged<CFError>?
-                    if !CTFontManagerRegisterGraphicsFont(font, &error) {
-                        print("Error registering font \(fontURL.lastPathComponent): \(String(describing: error))")
-                    } else {
-                        print("Successfully registered font: \(fontURL.lastPathComponent)")
-                    }
-                } else {
-                    print("Failed to load font at \(fontURL.lastPathComponent).")
-                }
+import UIKit
+import CoreText
+
+public class FontLoader {
+    public static func registerFonts() {
+        let bundle = Bundle.module
+        guard let fontURLs = bundle.urls(forResourcesWithExtension: "ttf", subdirectory: "Font/ttf") else {
+            print("No fonts found in the bundle.")
+            return
+        }
+        
+        for fontURL in fontURLs {
+            var error: Unmanaged<CFError>?
+            if CTFontManagerRegisterFontsForURL(fontURL as CFURL, .process, &error) == false {
+                print("Failed to load font: \(fontURL.lastPathComponent), error: \(String(describing: error))")
+            } else {
+                print("Font loaded successfully: \(fontURL.lastPathComponent)")
             }
-        } else {
-            print("No fonts found in the specified directory.")
         }
     }
 }
